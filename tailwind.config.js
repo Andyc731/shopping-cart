eb76782bcc95b48c60611c8b169f3f1463926b0f
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-01-30 15:36:26 -0700

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/*.jsx", "./index.html", "./src/components/*.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
