a8c62099971d86b40c433c32c3b84b7230106e27
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-01-31 15:05:31 -0700

    change div to Link for each product

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,32 +1,43 @@
 import { useEffect, useState } from "react";
 import SideBar from "./SideBar";
+import { Link } from "react-router-dom";
 
 function Products(props) {
-  const [displayProducts, setDisplayProducts] = useState([...props.products]);
+  const [displayProducts, setDisplayProducts] = useState([]);
+
+  useEffect(() => {
+    setDisplayProducts(props.products);
+  }, [props.products]);
+  console.log(props.products, displayProducts);
   return (
-    <main className="grid" style={{ gridTemplateColumns: "300px 1fr" }}>
-      <SideBar
-        products={props.products}
-        setProducts={setDisplayProducts}
-      ></SideBar>
-      <div
-        className="mb-auto grid gap-8 p-8"
-        style={{ gridTemplateColumns: "repeat(auto-fit, minmax(15rem, 1fr))" }}
-      >
-        {displayProducts.map((item, index) => {
-          return (
-            <div
-              key={index}
-              className="w-60 flex flex-col justify-between p-3 shadow-xl"
-            >
-              <img src={item.image} alt="" className="h-60 object-contain" />
-              <p className="mt-auto mb-0">{item.title}</p>
-              <p>{item.price}</p>
-            </div>
-          );
-        })}
-      </div>
-    </main>
+    displayProducts && (
+      <main className="grid" style={{ gridTemplateColumns: "300px 1fr" }}>
+        <SideBar
+          products={props.products}
+          setProducts={setDisplayProducts}
+        ></SideBar>
+        <div
+          className="mb-auto grid gap-8 p-7"
+          style={{
+            gridTemplateColumns: "repeat(auto-fit, minmax(15rem, 1fr))",
+          }}
+        >
+          {displayProducts.map((item, index) => {
+            return (
+              <Link
+                key={index}
+                to={`/products/${index + 1}`}
+                className="w-60 flex flex-col justify-between p-3 shadow-xl"
+              >
+                <img src={item.image} alt="" className="h-60 object-contain" />
+                <p className="mt-auto mb-0">{item.title}</p>
+                <p>{item.price}</p>
+              </Link>
+            );
+          })}
+        </div>
+      </main>
+    )
   );
 }
 
