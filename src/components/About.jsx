b43420bd50037a735e1a4d616346bea7620d5dfa
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-02-02 11:51:45 -0700

    add fillers

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,15 @@
 function About() {
-  return <h2>About</h2>;
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <h2>About</h2>
+      <p className="max-w-screen-sm">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+        quidem facilis, laborum unde magnam reiciendis accusamus tenetur
+        delectus voluptatibus nemo quasi laboriosam doloremque autem quae
+        corrupti odit hic? Consequatur, minima?
+      </p>
+    </div>
+  );
 }
 
 export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,17 @@
 import { Link } from "react-router-dom";
 
 function Home() {
-  return <h2>Home</h2>;
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <h2>Home</h2>
+      <Link
+        to={"/products"}
+        className="w-36 bg-blue-300 rounded-full h-10 flex justify-center items-center font-bold"
+      >
+        Shop Now
+      </Link>
+    </div>
+  );
 }
 
 export default Home;
